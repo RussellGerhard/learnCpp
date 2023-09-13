6a8f385007d1716a2399994fadc0d6c4254725f0
Russell Gerhard <russellgerhard1@gmail.com>
2023-09-12 22:36:43 -0400

    update tab length in format file

--- iostream/iostream.cpp
+++ iostream/iostream.cpp
@@ -1,27 +1,33 @@
 #include <iostream>
 
 int getAndPrintOneNumber() {
-  std::cout << "Entering getAndPrintOneNumber\n";
-  int x{};
-  std::cout << "Input a number: ";
-  std::cin >> x;
-  std::cout << "You entered: " << x << '\n';
-  return 0;
+    std::cout << "Entering getAndPrintOneNumber\n";
+    int x{};
+    std::cout << "Input a number: ";
+    std::cin >> x;
+    std::cout << "You entered: " << x << '\n';
+    return 0;
 }
 
 int getAndPrintTwoNumbers() {
-  std::cout << "Entering getAndPrintTwoNumbers\n";
-  int x{};
-  int y{};
-  std::cout << "Input two numbers separated by a space: ";
-  std::cin >> x;
-  std::cin >> y;
-  std::cout << "You entered: " << x << " and " << y << '\n';
-  return 0;
+    std::cout << "Entering getAndPrintTwoNumbers\n";
+    int x{};
+    int y{};
+    std::cout << "Input two numbers separated by a space: ";
+    std::cin >> x;
+    std::cin >> y;
+    std::cout << "You entered: " << x << " and " << y << '\n';
+    return 0;
+}
+
+int unspecifiedBehavior() {
+    std::cout << "Int size on this machine is: " << sizeof(int) << '\n';
+    return 0;
 }
 
 int main() {
-  getAndPrintOneNumber();
-  // getAndPrintTwoNumbers();
-  return 0;
+    getAndPrintOneNumber();
+    unspecifiedBehavior();
+    // getAndPrintTwoNumbers();
+    return 0;
 }
